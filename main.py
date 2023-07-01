cb5c35b1f96a8accd33ce1d4f1a8c14a27510581
Jaden <jadenvicaine@hotmail.com>
2023-07-02 01:53:36 +0200

    correction boutton centré

--- main.py
+++ main.py
@@ -41,17 +41,38 @@ ctk.deactivate_automatic_dpi_awareness()
 
 #Création des frames
 
+frame_useless = ctk.CTkFrame(window)
+frame_useless.pack(expand = True)
+
+
+
+
+
+frame_useless.grid_rowconfigure(0, weight = 1)    # Centre le cadre verticalement
+frame_useless.grid_columnconfigure(0, weight = 1) # Centre le cadre honrizontalement ta capté
 
 
 
 ##Création des widjets
 
-button_create_habit = ctk.CTkButton(window, text = "create a habit", width = 100, height = 50)
+button_create_habit = ctk.CTkButton(frame_useless , text = "create a habit", width = 100, height = 50)
+button_create_habit.pack()
+
+'''
 button_create_habit.place(relx=0.5, rely=0.4)
 
 
+Cela ne fonctionne pas chez moi, ce n'est pas centré, en gros c'est pas ouf mais ne t'inquiète pas, 
+j'ai la solution. Ah et dailleur je n ai eu aucun soucis pour pull, pense à utiliser stash au cas ou
+et stash pop pour retirer completement de la liste en gros stash joue le role d une corbeille mais 
+j'imagine que tu connais déjà. 
 
-button_create_habit.place()
+Cordialement
+
+Un débutant de git
+'''
+
+
 
 
 window.mainloop()
