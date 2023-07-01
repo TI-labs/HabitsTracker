d41128434e6813af27f5599f47aab541c5286a67
Tidjani Stefel <tidjani.stefel@outlook.com>
2023-07-02 00:36:31 +0200

    erase tkinter

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import customtkinter as ctk
 from Class import *
 import pandas as pd
 import numpy as np
 import datetime
 import tkinter as tk
 from function import *
+
 
 
 #Ouverture du fichier
@@ -28,17 +30,4 @@ def save_habits():
 
 #Création de la fenetre
 
-fenetre = tk.Tk()
-fenetre.config(background='#403F3F')
-fenetre.title('HabitTracker')
-fenetre.geometry("800x500")
-
-btn_creation1 = tk.Button(fenetre, text= "Create habits" )
-
-btn_creation1.pack(anchor='center' )
-
-
-
-
-fenetre.mainloop()
 
