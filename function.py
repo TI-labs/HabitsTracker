6e6f05d4ff78813d50d4c974c7d28346802f1d42
Jaden <jadenvicaine@hotmail.com>
2023-07-03 06:36:03 +0200

    amelioration update screen et creation de 2 fonctions, il reste a afficher le nouvel ecran en duplicant les elements de l'ancien car on ne peu pas changer le master d un widget

--- function.py
+++ function.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from os import stat
 import datetime
-
+import customtkinter as ctk
 
 
 
@@ -29,7 +29,42 @@ def est_fichier_vide(nom_fichier):
 
 
 
-
-
 #fonctions liées à l'interface
 
+
+
+#fonction get_widgets_and_frames
+#pour recuperer sous forme de liste les frames d'un top level et les widget inclu
+#dans chacun des frame     frames = [frame1,frame2]  Widgets = [[widget1_frame1 , widget2_frame1],
+                                                             #  [wiget1_frame2,  widget2_frame2]]  
+def get_widgets_and_frames(top_level):
+    widgets = []
+    frames = []
+
+    for x in top_level.winfo_children():
+        if isinstance(x, ctk.CTkFrame):
+            frames.append(x)
+            for widget in x.winfo_children():
+                widgets.append(widget)
+    
+        
+
+    return widgets, frames
+
+
+
+
+
+
+
+
+#fonction new_screen_disp
+#qui permet d'afficher un target_screen (préfait) sur une fenetre vide
+#target_screen est un ctk.CTkToplevel object
+
+           
+def new_screen_disp(window,target_screen):
+    widgets_target, frames_target = get_widgets_and_frames(target_screen)
+    print(widgets_target, frames_target)
+      
+            
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pandas as pd
 import datetime
 from function import *
 import customtkinter as ctk
-
+import tkinter as tk
 
 #Ouverture du fichier
 
@@ -26,12 +26,14 @@ def save_habits():
 
 
 
+
+
 #Création du premier ecran
 
 
-window1 = ctk.CTk()
-window1.title('first screen')
-window1.geometry("800x500")
+window = ctk.CTk()
+window.title('first screen')
+window.geometry("800x500")
 
 
 #choix du style + scaling
@@ -39,73 +41,64 @@ ctk.set_default_color_theme("themes/NightTrain.json")
 ctk.deactivate_automatic_dpi_awareness()
 
 
-#Création des frames
+#Création des frames premier ecran
 
-frame_main = ctk.CTkFrame(window1)
+frame_main = ctk.CTkFrame(window,width = 800,height = 500)
 frame_main.pack(expand = True)
 
-
-frame_child = ctk.CTkFrame(frame_main)
-frame_child.pack(expand=True)
-
-
-frame_main.grid_rowconfigure(0, weight = 1)    # Centre le cadre verticalement
-frame_main.grid_columnconfigure(0, weight = 1) # Centre le cadre honrizontalement ta capté
 
 
 
 
 #la fonction liée au boutton doit forcément être dans ce fichier
 
-def change_window(): #fonction permettant d'update une fenetre pour arriver sur une nouvelle fenetre
-    global window1
-    window1.after(ms=100)
-    window1.destroy()
-    window2.after(ms=300)
-    window2.mainloop()
-    ctk.deactivate_automatic_dpi_awareness()
+def change_window(): #fonction qui efface le contenu de la fenetre et affiche la page 2
+   
+    global window
     
-##je vais modifier cette fonction en faisant disparaitre les items du frame plutot que de créer une nouvelle fenetre,
-#ca sera plus fluide et il y aura moin de soucis je l'espère    
+    for widget in frame_main.winfo_children(): #on efface la premiere fenetre
+        widget.pack_forget()
+        
+        #pour que le frame prenne la couleur de la fenetre lorsque le boutton disparait
+        frame_main.configure(fg_color = window.cget('fg_color') )
+        window.title('2nd screen')
+        
+    #on utilise la fonction new_screen_disp(premiere_page,page_cible) pour afficher la page cible
+    new_screen_disp(window,screen2)
+        
+    
+    
     
 
     
-#je n'ai pas reussi à faire une fonction générale fonctionnant pour n'importe qu'elle fenetre a cause du comportement du boutton
+#Création des screens
+
+#attention, on utilise des toplevels et non des windows, les toplevels se comportent pareil
+
+screen2=ctk.CTkToplevel(window)
+screen2.withdraw()
+screen2.geometry("800x500")
+screen2.title('2nd screen')
 
 
-#si tu veux pour demain avant que j'arrive tu peux commencer à faire la fenetre 2 et j'améliorerais mon code si possible ou bien on peut faire l'inverse
+frame_bis = ctk.CTkFrame(screen2,width = 800, height = 500)
+frame_bis.pack()
+button_bis = ctk.CTkButton(frame_bis)
+button_bis.pack()
 
-
-#Création fenetre 2
-
-window2=ctk.CTk()
-window2.title('2nd screen')
-window2.geometry("800x500")
 
 
 
 
 #Création des widjets
 
-button_create_habit = ctk.CTkButton(frame_child , text = "create a habit", width = 100, height = 50, 
+button_create_habit = ctk.CTkButton(frame_main , text = "create a habit", width = 100, height = 50, 
                                     command = change_window)
+
 button_create_habit.pack()
 
-'''
-button_create_habit.place(relx=0.5, rely=0.4)
-
-
-Cela ne fonctionne pas chez moi, ce n'est pas centré, en gros c'est pas ouf mais ne t'inquiète pas, 
-j'ai la solution. Ah et dailleur je n ai eu aucun soucis pour pull, pense à utiliser stash au cas ou
-et stash pop pour retirer completement de la liste en gros stash joue le role d une corbeille mais 
-j'imagine que tu connais déjà. 
-
-Cordialement
-
-Un débutant de git
-'''
 
 
 
 
-window1.mainloop()
+window.mainloop()
