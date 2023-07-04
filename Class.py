fa1f96d2b842f17ac31dcf1860d0b7247252680b
Tidjani Stefel <tidjani.stefel@outlook.com>
2023-07-04 15:36:26 +0200

    creation de la class page

--- Class.py
+++ Class.py
@@ -10,6 +10,13 @@ class Habits :
         self.starting_date = starting_date
         self.attributs_ls = [name, iteration, starting_date]
 
+class Page :
+
+    def __init__(self):
+
+
+
+
 
     def add_iteration(self,booleen_value):
         #iteration est un tableau
