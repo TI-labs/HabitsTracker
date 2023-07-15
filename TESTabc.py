30c01cd2fcf402a5c977e0ba1138a61ead41889c
Jaden <jadenvicaine@hotmail.com>
2023-07-15 02:21:43 +0200

    reajustement fonction d heure plus ergonomique et animation tremblement

--- TESTabc.py
+++ TESTabc.py
@@ -26,9 +26,10 @@ def enter_event():
         habits_name_entry.unbind("<KeyPress-Return>")
         habits_name_entry.configure(state = 'disabled')
         window.after(300,lambda: start_transition(page, page2))
+        blink_function()
         enter_page2_1()
     else:
-        
+        habits_name_entry.unbind("<KeyPress-Return>")
         move_widgets2(habits_name_label)
         
 
@@ -99,7 +100,7 @@ def move_widgets2(widget):
         
         
         widget.place_configure(**initial_placement)
-        
+        habits_name_entry.bind("<KeyPress-Return>",lambda event: enter_event())
         
         
         
@@ -109,187 +110,302 @@ page = Page(window)
 page.configure(bg_color = window.cget('fg_color'))
 page.configure(border_color = 'white',border_width = 1)
 
-
-page2 = Page(window)
-page2.configure(border_color = 'white',border_width = 1)
-
-
-
-page3 = Page(window)
-page3.configure(border_color = 'white',border_width = 1)
-
-
-
-
-page.place(relheight = 1, relwidth = 1)
-
-
-
-
-
-
-#widgets
-
-
-habits_name_label = ctk.CTkLabel(page, text = 'Name it', font=('calibri',20,'bold'))
-
-habits_name_entry = ctk.CTkEntry(page,placeholder_text = "Brush my teeth",justify = "center")
-
-
-
-validation_time_label = ctk.CTkLabel(page2, text = 'Validation time', font = ('calibri',20,'bold'))
-
-
-
-
-
-
-def maj_pressed(event):    ##savoir si la  touche maj est activée
-    if event.state == 2  :  
-        print('la touche maj est activé')
-
-#entry event
-habits_name_entry.bind("<KeyPress-Return>" ,lambda event: enter_event())
-
-
-
-
-
-
-
-
-
-
-#placement page 1
-
-habits_name_label.place(relx = 0.5,rely = 0.3, anchor = 'center')
-
-habits_name_entry.place(relx = 0.5,rely = 0.4, anchor = 'center')
-
-
-#placement page 2
-
-validation_time_label.place(relx = 0.5,rely = 0.3, anchor = 'center')
-
-
-
-hours_1 = '0'
-hours_2 = '0'
-minutes_1 = '0'
-minutes_2 = '0'
-
-
-time_disp = ctk.CTkLabel(page2, text = str(hours_1) + str(hours_2) + ' : ' +
-                         str(minutes_1) + str(minutes_2),font = ('calibri',36))
-time_disp.place(relx = 0.5,rely = 0.4,anchor = "center")
-
-
-x_hours = 0 #cette variable pour savoir quel nombbre on modifie
-
-def check_entry(event): #event assoc à la modif de l heure 
-    global hours_1 , hours_2 , minutes_1 , minutes_2,x_hours
     
+def fonction_page2():
+    
+    page2 = Page(window)
+    page2.configure(border_color = 'white',border_width = 1)
+    
+    
+    
+    page3 = Page(window)
+    page3.configure(border_color = 'white',border_width = 1)
+    
+    
+    
+    
+    page.place(relheight = 1, relwidth = 1)
+    
+    
+    
+    
+    
+    
+    #widgets
+    
+    
+    habits_name_label = ctk.CTkLabel(page, text = 'Name it', font=('calibri',20,'bold'))
+    
+    habits_name_entry = ctk.CTkEntry(page,placeholder_text = "Brush my teeth",justify = "center")
+    
+    
+    
+    validation_time_label = ctk.CTkLabel(page2, text = 'Validation time', font = ('calibri',20,'bold'))
+    
+    
+    
+    
+    
+    
+    def maj_pressed(event):    ##savoir si la  touche maj est activée
+        if event.state == 2  :  
+            print('la touche maj est activé')
+    
+    
+    
+    #events
+    
+    habits_name_entry.bind("<KeyPress-Return>" ,lambda event: enter_event())
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #placement page 1
+    
+    habits_name_label.place(relx = 0.5,rely = 0.3, anchor = 'center')
+    
+    habits_name_entry.place(relx = 0.5,rely = 0.4, anchor = 'center')
+    
+    
+    #placement page 2
+    
+    validation_time_label.place(relx = 0.5,rely = 0.3, anchor = 'center')
+    
+    
+    
+    hours_1 = '0'
+    hours_0 = '0'
+    minutes_3 = '0'
+    minutes_2 = '0'
+    
+    
+    time_disp = ctk.CTkLabel(page2, text = str(hours_1) + str(hours_0) + ' : ' +
+                             str(minutes_3) + str(minutes_2),font = ('calibri',36),
+                             fg_color = 'transparent')
+    time_disp.place(relx = 0.5,rely = 0.4,anchor = "center")
+    
+    
+    
+    blink = "  "
     
     numbers = [str(x) for x in range(10)]
+    stock=['0','  ','0','0']
+    x=0
     
-    if event.keysym in numbers :
-        
-        if x_hours == 0:
+    first_try = True
+    
+    blind1_en_cours="on"
+    blind2_en_cours = "off"
+    
+    def checker():
+        global x, window, stock, first_try
+        if on_off_var == "off":
+            window.after(100,checker)
+        elif on_off_var == 'on':
             
+            if stock[0] in ["0","1","2"] and x == 0:
                 
-            hours_2 = event.keysym
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            x_hours += 1 
-            window.unbind("<Keypress>")
-            enter_page2_1()
+                x += 1
+            elif not stock[0] in ["0","1","2"] and x == 0 and first_try:
+                stock[1]="0"
+                x+=2
+            elif x == 1 and first_try == True: 
+                x += 1
+                
+                
+                blink_function()
+                
+            elif 1 < x and x < 3 and first_try == True:
+                x += 1
+                blink_function()
+                
+                
+            elif (x == 3 or x == 2) and first_try == False:
+                
+                x = 2
+                blink_function2()
+           
+            else:
+                blink_function()
             
-        elif x_hours == 1:
             
-            hours_1 = event.keysym
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            x_hours += 1 
-            enter_page2_1()
-            
-        elif x_hours == 2:
-            
-            minutes_2 = event.keysym
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            x_hours += 1 
-            enter_page2_1() 
-            
-        elif x_hours == 3:
-            
-            minutes_1 = event.keysym
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                     str(minutes_1) + str(minutes_2))
-            
-            enter_page2_1() 
-        window.unbind("<Keypress>")
-            
-    elif event.keysym == "BackSpace":
+    def checker2():
+        global x, window, stock
+        if on_off_var == "off":
+            window.after(100,checker)
         
-        
-        if x_hours == 0:
             
-            hours_2='0'
             
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            window.unbind("<Keypress>")
-            enter_page2_1()
             
-        elif x_hours == 1:
-            
-            hours_1 = "0"
-            x_hours -= 1
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            window.unbind("<Keypress>")
-            enter_page2_1()
-            
-        elif x_hours == 2:
-            
-            minutes_2 = '0'
-            x_hours -= 1
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            window.unbind("<Keypress>")
-            enter_page2_1()
-            
-        elif x_hours == 3:
-            
-            minutes_1 = '0'
-            x_hours -= 1
-            time_disp.configure(text = str(hours_1) + str(hours_2) + ' : ' + 
-                                    str(minutes_1) + str(minutes_2))
-            window.unbind("<Keypress>")
-            enter_page2_1()    
-    elif event.keysym == 'Return':
-       
-        if x_hours == 0 or x_hours == 1:
-            x_hours = 2
-            window.unbind("<Keypress>")
-            enter_page2_1()
-        
-        
-        
-        
-def enter_page2_1(): #quand on est dans la page 2 (fonction inutile mais on la garde)
-    if x_hours == 2 or x_hours == 3:
-        
-        window.bind('<KeyPress-Return>',enter_page2_2)
-
-
-
-    else:
-        window.bind("<KeyPress>", check_entry)
-
-def enter_page2_2(event):
-    window.unbind("<KeyPress>")
-    window.unbind('<KeyPress-Return>')
-    start_transition(page2, page3)
     
+        
+        
+        
+        
+    
+    def enter_page2_1(event=None):
+        global stock,numbers,x,on_off_var,first_try
+        window.bind('<KeyPress>', lambda event: enter_page2_1(event))
+        z = "no"
+        if event != None :
+                           
+            
+            if event.keysym in numbers:
+                    
+                if first_try == True: 
+                    
+                    if x == 1:
+                        swaper = stock[0]  
+                        stock[0] = event.keysym
+                        stock[1] = swaper
+                    
+                        
+                
+                        
+                    elif x == 3 and stock[2] in numbers[:6]:
+                        swaper = stock[2]  
+                        stock[2] = event.keysym
+                        stock[3] = swaper
+                        first_try = False
+                        
+                        
+                        
+    
+                        
+                      
+                    
+                    
+                    elif x == 3 and stock[2] in numbers[6:]:
+                        
+                        
+                        
+                        
+                        
+                        first_try = False
+                        
+                  
+                    
+                    
+                    else:
+                        
+                        stock[x]=event.keysym
+                        
+                    window.after(100,checker)
+                
+                elif first_try == False :
+                    
+                    if event.keysym in numbers[6:] and x == 3:
+                        blink_function2()
+                    
+                    
+                    
+                    stock[x]=event.keysym
+                    blink_function2()
+            elif event.keysym == "Left" and not first_try:
+                if x != 1:
+                    checker2()
+                    if x == 0 or x == 2:
+                        x += 1
+                    elif x == 3 : 
+                        x = 0
+            elif event.keysym == "Right" and not first_try:
+                if x != 2:
+                    checker2()
+                    if x == 1 or x == 3:
+                        x -= 1
+                    elif x == 0:
+                        x = 3
+            elif event.keysym == 'Return' and stock[0] in ["0","1","2"] and stock[1]in ["  ","0"]:
+                checker2()
+                x = 2
+                stock[1]="0"
+                time.sleep(0.3)
+        
+    
+    def blink_function():
+        global x, stock, on_off_var,blind2_en_cours,blind1_en_cours
+        
+        
+        if blind2_en_cours == "on":
+            window.after(100,checker)
+        
+        
+        elif blind2_en_cours == 'off':
+            
+            if on_off_var == "on":
+                    
+                    time_disp.configure(text=str(stock[1]) + str(stock[0]) + ' : ' +
+                                             str(stock[3]) + str(stock[2]))
+            elif on_off_var == 'off':
+                
+                
+                if x < 2 :
+                    time_disp.configure(text=str(blink) + str(blink) + ' : ' +
+                                             str(stock[3]) + str(stock[2]))
+                
+                else :
+                    time_disp.configure(text=str(stock[1]) + str(stock[0]) + ' : ' +
+                                             str(blink) + str(blink))
+            
+            window.after(300,blink_function)
+    
+    
+    def blink_function2():
+        global x, stock, on_off_var,blind2_en_cours,blind1_en_cours
+        blind2_en_cours = "on"
+        
+        if on_off_var == "on":
+            
+                time_disp.configure(text=str(stock[1]) + str(stock[0]) + ' : ' +
+                                         str(stock[3]) + str(stock[2]))
+        elif on_off_var == 'off':
+            
+            if x == 0 :
+                time_disp.configure(text=str(stock[1]) + str(blink) + ' : ' +
+                                         str(stock[3]) + str(stock[2]))
+            
+            elif x == 1 :
+                time_disp.configure(text=str(blink) + str(stock[0]) + ' : ' +
+                                         str(stock[3]) + str(stock[2]))
+            
+            elif x == 2 :
+                time_disp.configure(text=str(stock[1]) + str(stock[0]) + ' : ' +
+                                         str(stock[3]) + str(blink))
+            elif x == 3 :
+                time_disp.configure(text=str(stock[1]) + str(stock[0]) + ' : ' +
+                                         str(blink) + str(stock[2]))
+        
+        window.after(300,blink_function2)
+        
+            
+            
+            
+        def enter_page2_2(event):
+            window.unbind("<KeyPress>")
+            window.unbind('<KeyPress-Return>')
+            start_transition(page2, page3)
+        
+    
+    
+    on_off_var="on"
+    def on_off():
+        global on_off_var, window
+        if on_off_var == "on":
+            on_off_var="off"
+            window.after(630,on_off)
+        elif on_off_var == "off" :
+            on_off_var='on'
+            window.after(1600,on_off)
+            
+    on_off()
+        
+
+fonction_page2()
+
 window.mainloop()
